607c2ac7d243e3e5b3233afcb657531b58f64bc9
Ryan Woodring <rdwoodring@gmail.com>
2018-09-07 21:33:34 -0400

    getting some quick and dirty html and server routing bits out of the way in preparation for pulling in react. references #5

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,28 @@
 const express = require('express'),
-    router = express.Router();
+    router = express.Router(),
+    html = `
+        <html>
+            <head>
+                <title>
+                    Grimoire - Magic the Gathering Deck Builder
+                </title>
+            <head>
+
+            <body>
+                <div id="root">
+                    <!-- content here -->
+                </div>
+            </body>
+        </html>
+    `;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', {
-        title: 'Express'
-    });
+    // res.render('index', {
+    //     title: 'Express'
+    // });
+
+    res.send(html);
 });
 
 module.exports = router;
