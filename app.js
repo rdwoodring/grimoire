3109586ae018f4fae6bca667d1b932bcc10fd15d
Ryan Woodring <rdwoodring@gmail.com>
2018-09-07 20:55:59 -0400

    quick cleanup of scaffolded files to match the updated eslint rules

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-
-var app = express();
+const express = require('express'),
+    path = require('path'),
+    cookieParser = require('cookie-parser'),
+    logger = require('morgan'),
+    indexRouter = require('./routes/index'),
+    app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
