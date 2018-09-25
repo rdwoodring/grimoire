62c3e2046acaab6bd3fd8abaa61b9d77cd0c6026
Ryan Woodring <rdwoodring@gmail.com>
2018-09-24 20:59:35 -0400

    #6 - pull in router pieces in the app file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,22 @@
 import * as React from 'react';
+
 import './App.css';
 
-import logo from './logo.svg';
+import { Route, BrowserRouter, Switch } from "react-router-dom";
+import {Create} from './components/pages/Create';
+import {Home} from './components/pages/Home';
 
 class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    public render() {
+        return (
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/" component={Home}/>
+                    <Route exact path="/create" component={Create} />
+                </Switch>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
