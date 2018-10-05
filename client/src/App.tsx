90a4d08da4ef01fcc895996ad9a5212c97dbac6a
Ryan Woodring <rdwoodring@gmail.com>
2018-10-04 22:40:08 -0400

    #10 - updating route to confirm screen to actually be correct

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 
 import { Route, BrowserRouter, Switch } from "react-router-dom";
 import CreateContainer from './components/pages/create/CreateContainer';
+import ConfirmContainer from './components/pages/confirm/ConfirmContainer';
 import {Home} from './components/pages/home/Home';
 
 class App extends React.Component {
@@ -13,7 +14,7 @@ class App extends React.Component {
                 <Switch>
                     <Route exact path="/" component={Home}/>
                     <Route exact path="/create" component={CreateContainer} />
-                    <Route exact path="/confirm" component={CreateContainer} />
+                    <Route exact path="/confirm" component={ConfirmContainer} />
                 </Switch>
             </BrowserRouter>
         );
