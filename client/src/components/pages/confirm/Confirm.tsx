8f8dcd986b18b8cc6bff4bfc93c04855395c61fe
Ryan Woodring <rdwoodring@gmail.com>
2018-10-10 21:41:15 -0400

    #10 - adding a link button to the confirm page that points to the deck builder dashboard

--- client/src/components/pages/confirm/Confirm.tsx
+++ client/src/components/pages/confirm/Confirm.tsx
@@ -2,19 +2,27 @@ import * as React from 'react';
 
 import IConfirmPage from '../../../types/IConfirmPage';
 
+import Button from '../../form-elements/buttons/Button';
+import { Link } from 'react-router-dom';
 
 class Confirm extends React.Component<IConfirmPage> {
   public render() {
-    let fetchingMarkup;
+    let bodyMarkup;
 
     if (this.props.fetchingInitialCardList) {
-      fetchingMarkup = <div>Fetching</div>;
+        bodyMarkup = <div>Fetching</div>;
+    } else {
+        bodyMarkup =
+            <Link to="/deckbuilder">
+                <Button label="Confirm"  onClick={() => {}} />
+            </Link>
+        ;
     }
-    
+
     return (
       <div>
         <div>Confirm</div>
-        {fetchingMarkup}
+        {bodyMarkup}
       </div>
     );
   }
