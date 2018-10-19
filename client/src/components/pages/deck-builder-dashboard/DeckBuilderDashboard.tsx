8e77384f2d6b7270af2f22a125e1ee3e85afbb4c
Ryan Woodring <rdwoodring@gmail.com>
2018-10-18 21:57:04 -0400

    #13, #15 - start incorporating the listing/listing item into the dashboard page

--- client/src/components/pages/deck-builder-dashboard/DeckBuilderDashboard.tsx
+++ client/src/components/pages/deck-builder-dashboard/DeckBuilderDashboard.tsx
@@ -1,16 +1,22 @@
 import * as React from 'react';
 
+import Listing from '../../listings/Listing';
+import CardLine from '../../listing-items/CardLine';
 
 import IDeck from '../../../types/IDeck';
 import ICard from '../../../types/ICard';
 
 class DeckBuilderDashboard extends React.Component<IDeck> {
     public render() {
+        const cards: ICard[] = Array.from(this.props.cards.values());
 
         return (
             <div>
                 <div>
                     Deck Builder Dashboard
+                </div>
+                <div>
+                    <Listing itemData={cards} itemRenderer={(item: ICard) => {return <CardLine {...item} key={item.id} />;} } />
                 </div>
             </div>
         )
