47962fd533c14d8182333845b1e0e8382e00dfbc
Ryan Woodring <rdwoodring@gmail.com>
2019-01-28 22:44:39 -0500

    #22 - adding the endpoint routing middleware to the server app

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
 import logger from 'morgan';
 import dotenv from 'dotenv';
 
 import router from './routes/index';
+import api from './routes/api/v1/cards/index';
 
 const /* express = require('express'),
     path = require('path'),
@@ -21,8 +23,11 @@ app.use(express.urlencoded({
     extended: false
 }));
 app.use(cookieParser());
+app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../public')));
 
+app.use(api)
 app.use('/', router);
 
 // module.exports = app;
