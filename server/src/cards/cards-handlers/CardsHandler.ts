97ce65f07950b1048c1eb7ce008b88652aaf4746
Ryan Woodring <rdwoodring@gmail.com>
2019-01-28 22:41:22 -0500

    #22 - creating a cards handler that aggregates TCG and MTG IO responses

---
+++ server/src/cards/cards-handlers/CardsHandler.ts
@@ -0,0 +1,51 @@
+import ICardsHandler from './ICardsHandler';
+import IResultsMerger from '../results-mergers/IResultsMerger';
+
+import {AxiosResponse} from 'axios';
+
+class CardsHandler implements ICardsHandler {
+    subHandlers: Array<ICardsHandler>;
+    resultMerger: IResultsMerger;
+
+    constructor(subHandlers: Array<ICardsHandler>, resultMerger: IResultsMerger) {
+        this.subHandlers = subHandlers;
+        this.resultMerger = resultMerger;
+    }
+
+    getCards(cards: Array<string>): Promise<any> {
+        // return Promise.resolve();
+        const subHandlerPromises = this.subHandlers
+            .map((subHandler) => {
+                return subHandler.getCards(cards);
+            });
+
+        return Promise.all(subHandlerPromises)
+            .then((responses: Array<AxiosResponse>) => {
+                // const merged = responses[0].data
+                //     .results
+                //     .map((priceData: any) => {
+                //         const matchedCard = responses[1].data
+                //             .cards
+                //             .find((card: any) => {
+                //                 return card.name === priceData.productName && card.set === priceData.group.abbreviation;
+                //             }),
+                //             mergedCard = {
+                //                 ...matchedCard,
+                //                 ...priceData
+                //             };
+
+                //         return mergedCard;
+                //     });
+
+                // // console.log(merged);
+
+                // return {
+                //     results: merged,
+                //     totalResults: merged.length
+                // }
+                return this.resultMerger.mergeCards(responses[0], responses[1]);
+            });
+    }
+}
+
+export default CardsHandler;
